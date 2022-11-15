2ee3d2fadd581b56f22aefb3e7ad1b1b1542f7c4
Jordan J <labber@L115946.corp.ads>
2022-11-14 19:39:10 -0800

    new assertEqual file

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+//FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log("Assertion Passed: " + actual + "=== " + expected);
+  } else {
+    return console.log("Assertion Failed: " + actual + "!==" + expected);
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
