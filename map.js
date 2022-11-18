c22d9fda71670d1d0d89ce1889a33fe89ba96611
Jordan J <labber@L115946.corp.ads>
2022-11-17 18:40:53 -0800

    map.js

---
+++ map.js
@@ -0,0 +1,24 @@
+
+
+const map = function myFunc(array, callback) {
+
+  const results = [];
+
+  for (let item of array) {
+    results.push(callback(item));
+  }
+
+  return results;
+}
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+function regularFun(word) {
+  return word
+}
+
+const results1 = map(words, regularFun);
+console.log(results1)
+
+//map() is main function that takes 2 arguments, one an array and other with a callback function (regularFun)
+//when the map is called, are calling our callback function i.e. regularFun("ground");
